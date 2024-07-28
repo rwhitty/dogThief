66571d9eb22eee12fceef5ca3b747b85bbb480db
Ryan <rwhitty@berkeley.edu>
2024-07-27 18:16:42 -0700

    Shell scripts to run game

---
+++ run-game-local.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+ssh ec2-instance /home/ec2-user/projects/dogthief/run-game-server.sh"
--- run-game-server.sh
+++ run-game-server.sh
@@ -6,4 +6,6 @@ if [ -n "$PID" ]; then
   kill $PID
 fi
 
+git pull origin main
+
 python3 game/App.py
